94ed09a1adab6ea2c0ffe2f4bf68b837c80c5c5f
Mr-Yellow <mr-yellow@mr-yellow.com>
2017-07-20 07:24:01 +1000

    Almost

--- generator.py
+++ generator.py
@@ -28,7 +28,9 @@ class Generator():
 
     def recursive_division(self, cells, min_size, width, height, x=0, y=0):
         #print(width, height, x, y)
-        if width - x <= min_size * 2 or height - y <= min_size * 2:
+        #if width - x <= min_size * 2 or height - y <= min_size * 2:
+        #    return
+        if width <= min_size or height <= min_size:
             return
 
         # Choose axis to divide
@@ -45,7 +47,13 @@ class Generator():
             cut_size = width
             gap_size = height
 
+        if cut_size-min_size < min_size:
+            return
+        if gap_size-min_size < min_size:
+            return
+
         # Random division and doorway
+        print('cut/gap', min_size, cut_size-min_size, gap_size-min_size)
         cut = randint(min_size, cut_size-min_size)
         gap = randint(min_size, gap_size-min_size)
 
@@ -53,28 +61,36 @@ class Generator():
             print('small', cut, gap)
             return
 
+        print(x,y,cut)
+
         for i in xrange(0, gap_size):
             # TODO: make 1 depend on `door_size`
             #if abs(gap - i) > 1:
             if abs(gap - i) > 0:
                 # Copy wall tile from (0,0)
                 if axis == HORIZONTAL:
+                    print([x+i, y+cut])
                     cells[x+i][y+cut].tile = cells[0][0].tile
                 else:
+                    print([x+cut, y+i])
                     cells[x+cut][y+i].tile = cells[0][0].tile
 
-        print(x, y, cut, cut_size, gap, gap_size, 'H' if (axis == HORIZONTAL) else 'V')
+        print(x, y, [cut, gap], [cut_size, gap_size], 'H' if (axis == HORIZONTAL) else 'V')
 
 
         nx, ny = x, y
         w, h = [cut, height] if (axis == HORIZONTAL) else [width, cut]
         self.recursive_division(cells, min_size, w, h, nx, ny)
-        print('a', nx, ny, w, h)
+        #print('a', nx, ny, w, h)
+
+        nx, ny = [x-cut, y] if (axis == HORIZONTAL) else [x, y-cut]
+        w, h = [cut_size-cut, height] if (axis == HORIZONTAL) else [width, cut_size-cut]
+        self.recursive_division(cells, min_size, w, h, nx, ny)
 
         # FIXME: Wall in a doorway..
 
         #print(cut, cut_size, cut_size-cut)
-        nx, ny = [x+cut, y] if (axis == HORIZONTAL) else [x, y+cut]
-        w, h = [cut_size-nx, height] if (axis == HORIZONTAL) else [width, cut_size-ny]
-        self.recursive_division(cells, min_size, w, h, nx, ny)
-        print('b', nx, ny, w, h)
+        #nx, ny = [x+cut, y] if (axis == HORIZONTAL) else [x, y+cut]
+        #w, h = [cut_size-cut, height] if (axis == HORIZONTAL) else [width, cut_size-cut]
+        #self.recursive_division(cells, min_size, w, h, nx, ny)
+        #print('b', nx, ny, w, h)
