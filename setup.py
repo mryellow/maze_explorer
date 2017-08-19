7a17f0dddf0f1ea31ae3afa1116dd61d93b204b4
Mr-Yellow <mr-yellow@mr-yellow.com>
2017-08-19 13:57:25 +1000

    Bump 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='mazeexp',
-    version='0.0.6',
+    version='0.0.7',
     author='Mr-Yellow',
     author_email='mr-yellow@mr-yellow.com',
     description='A maze exploration game engine',
